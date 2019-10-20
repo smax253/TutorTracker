085846e52f273c45d642eaf5f90a387382b95b00
Kevin Li <46378390+adularescence@users.noreply.github.com>
2019-10-20 02:10:39 -0400

    feat: tutor-picking-button fetches from endpoint and updates itself (although not with the fetched data)

--- frontend/src/components/TutorButton.vue
+++ frontend/src/components/TutorButton.vue
@@ -1,5 +1,8 @@
+/**
+ * Responsible for hailing a tutor.
+ */
 <template>
-  <button>Toucher moi!</button>
+  <button @click="pressed">{{ this.courseName }}</button>
 </template>
 
 <script>
@@ -7,7 +10,24 @@ export default {
   name: "TutorButton",
   data() {
     return {
-
+      // "field"
+      tutorCount: 1,
+      courseName: "CHEM1101"
+    }
+  },
+  methods: {
+    pressed() {
+      //fetch('https://jsonplaceholder.typicode.com/todos/1')
+      fetch('https://tutortracker.appspot.com/api/tutor')
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          this.$emit("courseName", this.courseName);
+          this.tutorCount--;
+          if (this.tutorCount == 0) {
+            this.courseName = "No more tutors!";
+          }
+        });
     }
   }
 }
